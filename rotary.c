dc9bfdd7b2b78cb986be8b1ef72bc33e9a26c64b
Clemens Hauser <clemens.hauser@posteo.de>
2022-08-26 13:24:19 +0200

    Activated button-pullup, optimized datatypes, added getSwitch()

--- rotary.c
+++ rotary.c
@@ -15,18 +15,25 @@ void rotary_init()
 {	
 	status = 0;
 	cnt = 0;
-	button = 0;
-			
+	
+	button_status = 0;
+	button_flag = false;
+	button_switch = false;
+		
 	TCCR1A =	(1<<COM1A0);
 	TCCR1B =	TIMER_CONTROL;			//F_TIMER = 125 kHz -> for F_INT = 1 kHz count to 125-1 (interrupt fired 1 cycle later)
 	OCR1A  =	TIMER_COMPARE;
 	
 	TIMSK |= (1<<OCIE1A);										//Enable Output compare match interrupt enable (@TIMER_COMPARE)
 	
+	
+	SIGNAL_DDR &= (0<<SIGNAL_A) & (0<<SIGNAL_B) & (0<<SW);		//Set all as input
+	SIGNAL_PORT |= (1<<SW);										//Enable internal pullup for SW-Button
+	
 	status |=	(SIGNAL_PIN & (1<<SIGNAL_A))>>(SIGNAL_A-1) ;		//write new status
 	status |=	(SIGNAL_PIN & (1<<SIGNAL_B))>>(SIGNAL_B) ;		//write new status
 	
-	button =	(SIGNAL_PIN & (1<<SW))>>SW;						//Push button status
+	button_status |=	(SIGNAL_PIN & (1<<SW))>>SW;						//Push button status
 	
 	sei();														//start global interrupts
 	
@@ -49,24 +56,44 @@ int8_t rotary_getChange()
 	return temp;	
 }
 
-bool rotary_getButton()
+uint8_t rotary_getButton()
 {
-	return button;
+	return button_status;
 }
 
-
+bool rotary_getSwitch()
+{
+	return button_switch;
+}
 
 ISR (TIMER1_COMPA_vect)											//executing every 1ms
 {	
+	//**** ROTARY EVALUATION ****
 	status = status << 2;										//shift to make place for new status
 	status |=  (SIGNAL_PIN & (1<<SIGNAL_A))>>(SIGNAL_A-1) ;		//write new status
 	status |=  (SIGNAL_PIN & (1<<SIGNAL_B))>>(SIGNAL_B) ;		//write new status
-	
-	button =	(SIGNAL_PIN & (1<<SW))>>SW;						//Push button status
-	
+
 	if((status & 0x0F) == 0xB || (status & 0x0F) == 0x4)
 			cnt ++;	
 				
 	if((status & 0x0F) == 0x7 || (status & 0x0F) == 0x8)
 			cnt --;  
+			
+	//**** BUTTON EVALUATION ****
+	
+	button_status = button_status << 1;
+	button_status |=	(SIGNAL_PIN & (1<<SW))>>SW;					//write new push button status
+	
+	if(!(button_status&0x1)  &&  ((button_status&0x2)>>1))			//falling edge of button
+	{
+		button_flag = true;											//button was pushed
+	}
+	
+	if(button_flag && (button_status&0x1))							// button released and pushed before
+	{	
+		// ****switch toggled****
+		button_flag = false;										// reset flag
+		button_switch = !button_switch;								// toggle switch			
+	}
+	
 }
--- rotary.h
+++ rotary.h
@@ -18,23 +18,28 @@
 
 #define SIGNAL_DDR			DDRD
 #define	SIGNAL_PIN			PIND 
+#define SIGNAL_PORT			PORTD
 #define SIGNAL_A			PD7
 #define SIGNAL_B			PD6
-#define SW					PD4
+#define SW					PD5
 
 #define F_CPU		8000000
 
 #define TIMER_CONTROL	0x0B					//Prescaler 64, clear timer on compare match active
 #define TIMER_COMPARE	124
 
-static volatile char status;					//working register to save current and old signal status
-static volatile int8_t cnt;						//counter for rotary operation
-static volatile bool button;					//status of push button
+static volatile uint8_t status;						//working register to save current and old signal status
+static volatile int8_t cnt;							//counter for rotary operation
+
+static volatile uint8_t button_status;					//working register of push button
+static volatile bool button_flag;							//working register of push button
+static volatile bool button_switch;							
 
 void rotary_init();
 
 int8_t rotary_getCount();						//returns cnt 
 int8_t rotary_getChange();						//returns cnt change since last call
-bool rotary_getButton();
+uint8_t rotary_getButton();						//returns last button states
+bool rotary_getSwitch();						//returns switch status
 
 #endif /* ROTARY_H_ */
